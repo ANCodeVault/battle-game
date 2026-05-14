e3c20151609b31fb78b3bc1c715bd95193965fb8
hardzero <wixphp@gmail.com>
2026-05-14 12:44:03 +0300

    delete: удалил функцию debug

--- index.php
+++ index.php
@@ -6,11 +6,4 @@ error_reporting(-1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-function debug($arr): void
-{
-    echo '<pre>' . PHP_EOL;
-    var_dump($arr);
-    echo '</pre>' . PHP_EOL;
-}
-
 echo new \App\Game()->run();
